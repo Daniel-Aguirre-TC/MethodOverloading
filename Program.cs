5306ca39d18b89624ddba8fbbdae8a5ab53a6e26
Daniel Aguirre <uhyeay24@gmail.com>
2021-08-28 08:01:26 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1, 5));
+            Console.WriteLine(Add(2, 5, true));
+            Console.WriteLine(Add(2.40, 3.7));
+        }
+
+        // Add() with two int params.
+        static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        // Add() with two double params.
+        static double Add(double numOne, double numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        // Add() with two int params and a bool
+        static string Add(int numOne, int numTwo, bool isDollars)
+        {      
+            // if bool is true
+            if (isDollars)
+            {
+                // store sum
+                int sum = numOne + numTwo;
+                // return sum + "dollars" or "dollar"
+                return sum + (sum > 1 ? " dollars" : "dollar");
+            }
+            // if bool is not true, just return a string stating the sum.
+            else return Add(numOne, numTwo).ToString();
+        }
+
+    }
+}
